2e1408f493c692298b038f84127dd0c1c98039c5
Amber Stevenson <amber.stevenson14@gmail.com>
2023-03-17 16:48:34 -0600

    first commit

--- Lottery Picker/LotteryPickerViewController.swift
+++ Lottery Picker/LotteryPickerViewController.swift
@@ -11,22 +11,19 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    
-    
-    
     @IBOutlet weak var powerballOne: UILabel!
     @IBOutlet weak var powerballTwo: UILabel!
     @IBOutlet weak var powerballThree: UILabel!
-    @IBOutlet weak var powerballFour: UILabel!
-    @IBOutlet weak var powerballFive: UILabel!
     @IBOutlet weak var powerballSix: UILabel!
+    @IBOutlet weak var powerballFive: UILabel!
+    @IBOutlet weak var powerballFour: UILabel!
     
-    var randomNumber = 2
-    var randomNumber2 = 2
-    var randomNumber3 = 2
-    var randomNumber4 = 2
-    var randomNumber5 = 2
-    var randomNumber6 = 2
+    var randomNumber = 1
+    var randomNumber2 = 0
+    var randomNumber3 = 0
+    var randomNumber4 = 0
+    var randomNumber5 = 0
+    var randomNumber6 = 0
     
 
     override func viewDidLoad() {
@@ -37,25 +34,23 @@ class ViewController: UIViewController {
     @IBAction func pickMyWinnersTapped(_ sender: UIButton) {
         print("pushed")
 
-        
-        
-        randomNumber = Int(arc4random_uniform(71))
+        randomNumber = Int(arc4random_uniform(70))
         powerballOne.text = String(randomNumber)
         
-        randomNumber = Int(arc4random_uniform(71))
-        powerballTwo.text = String(randomNumber)
+        randomNumber2 = Int(arc4random_uniform(71))
+        powerballTwo.text = String(randomNumber2)
         
-        randomNumber = Int(arc4random_uniform(71))
-        powerballThree.text = String(randomNumber)
+        randomNumber3 = Int(arc4random_uniform(71))
+        powerballThree.text = String(randomNumber3)
         
-        randomNumber = Int(arc4random_uniform(71))
-        powerballFour.text = String(randomNumber)
+        randomNumber4 = Int(arc4random_uniform(71))
+        powerballFour.text = String(randomNumber4)
         
-        randomNumber = Int(arc4random_uniform(71))
-        powerballFive.text = String(randomNumber)
+        randomNumber5 = Int(arc4random_uniform(71))
+        powerballFive.text = String(randomNumber5)
 
-        randomNumber = Int(arc4random_uniform(26))
-        powerballSix.text = String(randomNumber)
+        randomNumber6 = Int(arc4random_uniform(26))
+        powerballSix.text = String(randomNumber6)
     
     }
     
